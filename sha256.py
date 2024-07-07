adc4dee1093e25f2b80b729f87ef1c2b5ee28421
0bscenity <154705783+0bscenity@users.noreply.github.com>
2024-07-06 22:09:38 -0500

    Update sha256.py

--- sha256.py
+++ sha256.py
@@ -1,8 +1,8 @@
-print(f'made by grapes')
+print('made by grapes')
 import os
 import hashlib
 import csv
-
+directory_to_scan =input('please input FULL directory path, or type exit to quit: ')
 def calculate_sha256(file_path):
     sha256 = hashlib.sha256()
     with open(file_path, "rb") as f:
@@ -13,7 +13,7 @@ def calculate_sha256(file_path):
 
 def scan_directory(directory):
     """scan"""
-    print(f"scanning directory and calculating the hash(es)")
+    print('scanning {directory_to_scan} and calculating the hash(es)')
     results = []
     for root, _, files in os.walk(directory):
         for file in files:
@@ -24,7 +24,7 @@ def scan_directory(directory):
 
 
 def export_to_csv(results, output_file):
-    print(f"exporting")
+    print('exporting')
     """export"""
     with open(output_file, mode="w", newline="", encoding="utf-8") as csv_file:
         writer = csv.writer(csv_file)
@@ -32,13 +32,10 @@ def export_to_csv(results, output_file):
         for file_name, sha256 in results:
             writer.writerow([file_name, sha256])
 
-
 if __name__ == "__main__":
     """variables"""
-    # keep r string otherwise it won't scan
-    directory_to_scan = r"C:\Users\XXXXXXXX"  # change directory. might only work if this script is on the same drive as path
     output_csv_file = "output_sha256.csv"
     results = scan_directory(directory_to_scan)
     export_to_csv(results, output_csv_file)
 
-    print(f"list has been exported to {output_csv_file}")
+    print(f'list has been exported to {output_csv_file}')
