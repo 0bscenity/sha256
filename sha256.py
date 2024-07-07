e8e778b3b846e0e1482d3a6016357740dab6bba1
0bscenity <154705783+0bscenity@users.noreply.github.com>
2024-07-07 02:19:07 -0500

    Update sha256.py

--- sha256.py
+++ sha256.py
@@ -2,18 +2,22 @@ print('made by grapes')
 import os
 import hashlib
 import csv
-directory_to_scan =input('please input FULL directory path, or type exit to quit: ')
+directory_to_scan = input('please input FULL directory path, DO NOT INCLUDE "⧵" AT THE END OF THE PATH OR IT WILL NOT WORK! type exit to quit: ')
+output_csv_file = input('name of the output file (ex, sha256.csv): ')
+
+if not output_csv_file.endswith('.csv'):
+    output_csv_file += '.csv'
+
 def calculate_sha256(file_path):
     sha256 = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
             sha256.update(chunk)
     return sha256.hexdigest()
 
 
 def scan_directory(directory):
-    """scan"""
-    print('scanning {directory_to_scan} and calculating the hash(es)')
+    print(f'scanning {directory} and calculating the hash(es)')
     results = []
     for root, _, files in os.walk(directory):
         for file in files:
@@ -25,16 +29,14 @@ def scan_directory(directory):
 
 def export_to_csv(results, output_file):
     print('exporting')
-    """export"""
-    with open(output_file, mode="w", newline="", encoding="utf-8") as csv_file:
+    with open(output_file, mode='w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(["file name", "sha256"])
+        writer.writerow(['file name', 'sha256'])
         for file_name, sha256 in results:
             writer.writerow([file_name, sha256])
 
-if __name__ == "__main__":
-    """variables"""
-    output_csv_file = "output_sha256.csv"
+if __name__ == '__main__':
+    ##output_csv_file = 'output_sha256.csv'
     results = scan_directory(directory_to_scan)
     export_to_csv(results, output_csv_file)
 
