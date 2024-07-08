912fc16f606b2254a0436d9d1ac00639eaf3ed3f
0bscenity <154705783+0bscenity@users.noreply.github.com>
2024-07-08 16:03:00 -0500

    Add files via upload
    reduced size by ~250 bytes

--- sha256.py
+++ sha256.py
@@ -1,14 +1,12 @@
-print('made by grapes')
-import os
-import hashlib
-import csv
-directory_to_scan = input('please input FULL directory path, DO NOT INCLUDE "⧵" AT THE END OF THE PATH OR IT WILL NOT WORK! type exit to quit: ')
-output_csv_file = input('name of the output file (ex, sha256.csv): ')
+print('made by grapes🍇')
+import os,hashlib,csv
+directory_2_scan = input('please input FULL directory path, DO NOT INCLUDE "⧵" AT THE END OF THE PATH OR IT WILL NOT WORK!: ')
+csv_out = input('name of the output file (ex, sha256.csv): ')
 
-if not output_csv_file.endswith('.csv'):
-    output_csv_file += '.csv'
+if not csv_out.endswith('.csv'):
+    csv_out += '.csv'
 
-def calculate_sha256(file_path):
+def calc_sha256(file_path):
     sha256 = hashlib.sha256()
     with open(file_path, 'rb') as f:
         for chunk in iter(lambda: f.read(4096), b''):
@@ -16,18 +14,18 @@ def calculate_sha256(file_path):
     return sha256.hexdigest()
 
 
-def scan_directory(directory):
+def scan_dir(directory):
     print(f'scanning {directory} and calculating the hash(es)')
     results = []
     for root, _, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
-            sha256 = calculate_sha256(file_path)
+            sha256 = calc_sha256(file_path)
             results.append((file, sha256))
     return results
 
 
-def export_to_csv(results, output_file):
+def export_2_csv(results, output_file):
     print('exporting')
     with open(output_file, mode='w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
@@ -36,8 +34,8 @@ def export_to_csv(results, output_file):
             writer.writerow([file_name, sha256])
 
 if __name__ == '__main__':
-    ##output_csv_file = 'output_sha256.csv'
-    results = scan_directory(directory_to_scan)
-    export_to_csv(results, output_csv_file)
+#     uncomment if you want to remove the user choise of output file name. csv_out = 'output_sha256.csv'
+    results = scan_dir(directory_2_scan)
+    export_2_csv(results, csv_out)
 
-    print(f'list has been exported to {output_csv_file}')
+    print(f'list has been exported to {csv_out}')
